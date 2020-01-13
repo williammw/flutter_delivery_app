0540f29c20ac4d913417475a8dd8f3d731158539
wmwwong <wmwwong@legco.gov.hk>
2020-01-13 16:23:46 +0800

    home_screen.dart and recent_orders.dart added

---
+++ lib/widgets/recent_orders.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/data/data.dart';
+import 'package:flutter_food_delivery_ui/models/order.dart';
+
+class RecentOrders extends StatelessWidget {
+  _buildRecentOrder(BuildContext context, Order order) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      width: 320.0,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15.0),
+          border: Border.all(
+            width: 1.0,
+            color: Colors.grey[200],
+          )),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          child: Text(
+            'Recent Order ',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 1.2,
+            ),
+          ),
+          padding: EdgeInsets.symmetric(
+            horizontal: 20.0,
+          ),
+        ),
+        Container(
+            height: 120.0,
+//            color: Colors.blue,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: currentUser.orders.length,
+              itemBuilder: (BuildContext context, int index) {
+                Order order = currentUser.orders[index];
+                return _buildRecentOrder(context, order);
+              },
+            ))
+      ],
+    );
+  }
+}
